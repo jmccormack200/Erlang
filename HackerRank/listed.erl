8758728c0c008bf7c8272c9396fd611c622e0cfe
John McCormack <mccormack.wgsi@gmail.com>
2015-09-07 17:38:14 -0400

    Finished the Hackerrank Problem entitled 'List Replication' In their Functional Programming Tract

--- HackerRank/listed.erl
+++ HackerRank/listed.erl
@@ -8,8 +8,14 @@ print_again(Repeat,Number) ->
     print_again(Repeat, Number, Repeat).
 
 print_again(0,_,Repeat) ->
-    {ok,[New_Number]} = io:fread("", "~d"),
-    print_again(Repeat,New_Number,Repeat);
+    try 
+       {ok,[New_String]} = io:fread("", "~s"),
+       {New_Number, _} = string:to_integer(New_String),
+       print_again(Repeat,New_Number,Repeat)
+    catch
+        _:Exception -> ok
+    end;
+
 
 print_again(Iteration,Number,Repeat) when Number >= 1, Number =< 100 ->
     io:format("~p~n",[Number]),
@@ -21,3 +27,4 @@ print_again(_,_,_) ->
 main() ->
         {ok, [Repeat, Number]} = io:fread("", "~d~d"),
         print_again(Repeat,Number).
+
